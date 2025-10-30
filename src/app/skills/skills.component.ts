a2ff8251b9bf07cef8cb83e07c019bf35af8e20f
Smookerzz <ziad.oneday@gmail.com>
2025-10-30 21:28:54 +0100

    reducing presented data on my website

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -11,30 +11,68 @@ export class SkillsComponent implements OnInit {
 
   data = {
     "Software Development": {
-      "Knowledge": [['Back-end Development', 5], ['Front-end Development', 4], ['Web Security', 4], ['MVC Architecture', 5], ['REST API', 5], ['Distributed System Programming', 3], ['Design Patterns', 4]],
-      "Programming Language": [['Java', 5], ['Kotlin', 4], ['JavaScript', 5],['Python',4], ['TypeScript',4], ['C/C++',3], ['Assembly',2]],
-      "Libraries":[['OpenMP', 3], ['MPI', 3], ['JavaThread', 4], ['ModbusTCP', 4], ['Java Socket', 5], ['Java Awt / Swing / FX', 4]],
-      'Back-end Frameworks': [['Spring Boot',5], ['JEE',4], ['Hibernate/JPA',5], ['Django',3]],
-      'Back-end Libraries': [['Spring Modules',5], ['Selenium', 4],['JUnit 5',5]],
-      "Front-end Frameworks":[["React Native for Android / IOS",3],["Angular",5],['Bootstrap',5], ['Canvas', 5]],
-      'Front-end Libraries and technologies': [["Java Server Pages",5],["JSTL",5],["JQuery", 4], ["AJAX", 4],["HTML5", 5], ["CSS3", 5]],
-      'Scripting Languages': [["Bash",5]],
-      'IDE': [['Eclipse', 5], ['Intellij', 4], ['STS', 5], ['Webstorm', 5], ['VS Code', 5], ['PyCharm', 4], ['Android Studio', 3], ['Entreprise Architect', 5], ['Postman', 4]],
-      'Database Management': [['Oracle DB', 3], ['MySQL', 5], ['PostgreSQL', 4], ['MariaDB', 3],['SQL',4]],
-      'Modeling Language': [['UML', 5],['Merise',5]],
-      'Automation Tools / Package Manager': [['Maven for Java',5], ['NodeJS npm',4], ['Python PIP',5],['Gradle for Java',1]],
-      'Tools And CI/CD': [['Gitlab',3]],
-      'Version Control': [['Git',5]],
-      'Servers': [['Apache Tomcat',5]]
-    },
-    'AI Technologies': {
-      "Knowledge": [['Machine Learning',4], ['Deep Learning',3], ['Computer Vision',3]],
-      "Programming Language": [['Python',4], ['R',2]],
-      "Libraries": [['Pandas',4], ['Numpy',4], ['OpenCV',3], ['Seaborn',2], ['Matplotlib',5], ['Gapminder',2]],
-      'Frameworks': [['Tensorflow',3], ['Scikit-Learn',4]],
-      'IDE': [['Jupyter',5], ['VS Code',3], ['PyCharm',5]]
+        "Knowledge": [
+            ['Back-end Development', 4],
+            ['Front-end Development', 3],
+            ['Web Security', 2],
+            ['REST API', 3]
+        ],
+        "Programming Languages": [
+            ['Java', 3.5],
+            ['Kotlin', 2],
+            ['JavaScript', 2.5],
+            ['TypeScript', 2.5],
+            ['Python', 4],
+            ['C/C++', 2],
+            ['Assembly', 1],
+            ['Bash', 2.5]
+        ],
+        "Frameworks": [
+            ['Spring / Spring Cloud', 4],
+            ['JEE', 3],
+            ['Django', 2],
+            ['Angular 13', 3],
+            ['Bootstrap', 2.5]
+        ],
+        "Libraries and Technologies": [
+            ['JSP / JSTL', 4],
+            ['jQuery', 2],
+            ['HTML / CSS', 5],
+            ['Canvas Drawing in Code', 2]
+        ],
+        "Testing Tools": [
+            ['JUnit 5', 3.5],
+            ['Selenium', 3],
+            ['Mockito', 2]
+        ],
+        "IDE / Tools": [
+            ['Eclipse / STS', 4.5],
+            ['IntelliJ', 3.5],
+            ['WebStorm', 3],
+            ['PyCharm', 3],
+            ['Postman', 2.5]
+        ],
+        "Databases": [
+            ['MySQL', 3],
+            ['Oracle DB', 3],
+            ['SQL Syntax', 4]
+        ],
+        "Modeling Languages": [
+            ['UML (Software Enterprise Architect)', 4],
+            ['Merise', 3]
+        ],
+        "DevOps / Tools": [
+            ['Git', 4],
+            ['GitHub Workflows', 2],
+            ['Docker / Docker Compose', 3],
+            ['Jenkins (Usage Only)', 1],
+            ['Maven', 3],
+            ['npm (NodeJS)', 3],
+            ['pip (Python)', 2]
+        ]
     }
-  };
+}
+
 
   constructor() { }
 
