47008f8d9d49ead0c8b56806e46aea78e4d7a641
Smookerzz <ziad.oneday@gmail.com>
2025-11-05 21:58:55 +0100

    P-7: remove education module

--- src/app/Routes.ts
+++ src/app/Routes.ts
@@ -1,12 +1,10 @@
 import {Routes} from "@angular/router";
 import {ResumeComponent} from "./resume/resume.component";
 import {HomeComponent} from "./home/home.component";
-import {EducationComponent} from "./education/education.component";
 import {ProjectsComponent} from "./projects/projects.component";
 
 export const routes:Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'abstract', component: ResumeComponent },
-  { path: 'education', component: EducationComponent },
   { path: 'projects', component: ProjectsComponent }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,6 @@ import { FormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
 import { HomeComponent } from './home/home.component';
 import { ResumeComponent } from './resume/resume.component';
-import { EducationComponent } from './education/education.component';
-import { FhaacheninformationsComponent } from './education/fhaacheninformations/fhaacheninformations.component';
-import { UmiinformationsComponent } from './education/umiinformations/umiinformations.component';
 import { routes } from "./Routes";
 import {NgOptimizedImage} from "@angular/common";
 
@@ -19,10 +16,7 @@ import {NgOptimizedImage} from "@angular/common";
     AppComponent,
     ProjectsComponent,
     HomeComponent,
-    ResumeComponent,
-    EducationComponent,
-    FhaacheninformationsComponent,
-    UmiinformationsComponent
+    ResumeComponent
   ],
   imports: [
     BrowserModule,
