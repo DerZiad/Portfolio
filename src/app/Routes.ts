13a5d39e6b5274a3d431a78c0b877e0b1d457302
Ziad Bougrine <40337316+DerZiad@users.noreply.github.com>
2025-11-09 03:42:43 +0100

    P-6 : refactor education section and integrate all information into resume (#12)
    * P-6: Integrate education module
    
    * P-6: Redesign education module

--- src/app/Routes.ts
+++ src/app/Routes.ts
@@ -2,9 +2,11 @@ import {Routes} from "@angular/router";
 import {ResumeComponent} from "./resume/resume.component";
 import {HomeComponent} from "./home/home.component";
 import {ProjectsComponent} from "./projects/projects.component";
+import {EducationComponent} from "./education/education.component";
 
 export const routes:Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'resume', component: ResumeComponent },
+  { path: 'education', component: EducationComponent },
   { path: 'projects', component: ProjectsComponent }
 ];
